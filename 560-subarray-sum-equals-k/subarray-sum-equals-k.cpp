6f23a8790699352567025c071d8b6dfdb92ddcb4
Ashika Maheshwari <ashikamaheshwari.sde@gmail.com>
2026-06-21 13:00:15 +0530

    Time: 30 ms (96.88%) | Memory: 46.9 MB (30.64%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,16 +5,43 @@ public:
         int n=nums.size();
         int count=0;
 
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum=sum+nums[j];
-                if(sum==k){
-                    count++;
+        // BRUTE FORCE
+        //for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum=sum+nums[j];
+        //         if(sum==k){
+        //             count++;
                     
-                }
-            }
+        //         }
+        //     }
+        // }
+        // return count;
+
+        vector<int>prefixsum(n,0);
+        prefixsum[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            prefixsum[i]=prefixsum[i-1]+nums[i];
         }
-        return count;
+
+        for(int j=0;j<n;j++){
+            if(prefixsum[j]==k) count++;
+        }
+
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            int val=prefixsum[i]-k;
+
+            if(mp.find(val)!=mp.end()){
+                count=count+mp[val];
+            }
+            mp[prefixsum[i]]++;
+        }
+
+
+    return count;
+
     }
 };
