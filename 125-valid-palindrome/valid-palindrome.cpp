7b062c12b51cdfd40d02f66b03b41f9696ee20fa
Ashika Maheshwari <ashikamaheshwari.sde@gmail.com>
2026-07-02 23:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (55.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<int> res1;
         int n=s.size();
-        for(int i=0;i<n;i++){
-            s[i]=tolower(s[i]);
-
-        }
-        for(int i=0;i<n;i++){
-            if((int(s[i])>=97 && int(s[i])<=122) || (s[i]>='0' &s[i]<='9')){
-                res1.push_back(s[i]);
-            }
-        }
-        int m= res1.size();
         int left=0;
-        int right=m-1;
-        bool palindrome=true;
+        int right=n-1;
+
         while(left<right){
-            if(res1[left]!=res1[right]) palindrome = false;
+           while(left<right && !isalnum(s[left])){
+            left++;
+           }
+
+           while(left<right && !isalnum(s[right])){
+            right--;
+           }
+
+            if (tolower(s[left]) != tolower(s[right]))
+                return false;
+
             left++;
             right--;
-        }
-    return palindrome;
 
+
+        }
+        return true;
     }
 };
