1648ae838b0f4a06390defe407023392c4152f71
Ashika Maheshwari <ashikamaheshwari.sde@gmail.com>
2026-08-09 16:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.57%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if(n<=4) return 0;
+        int ans=0;
+
+        while(n/5>=1){
+            ans+=n/5;
+            n=n/5;
+
+
+        }
+return ans;
+    }
+    
+};
